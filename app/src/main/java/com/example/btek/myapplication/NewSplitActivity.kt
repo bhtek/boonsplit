5eb0fafb87d206e165c9fb56b871df964b0fec58
Boon Hian Tek <boon.tek@gmail.com>
2018-01-23 22:29:23 +0800

    Initial check in. Will refactor to match names.

---
+++ app/src/main/java/com/example/btek/myapplication/NewSplitActivity.kt
@@ -0,0 +1,12 @@
+package com.example.btek.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewSplitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_split)
+    }
+}

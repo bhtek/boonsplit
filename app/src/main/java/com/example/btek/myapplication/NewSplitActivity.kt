b3f236e672c94c0322dad976d927e83e30f8b2fa
mantis78 <boon.tek@gmail.com>
2018-01-28 00:22:01 +0800

    [BT] Add capture for name and ready for main activity.

--- app/src/main/java/com/example/btek/myapplication/NewSplitActivity.kt
+++ app/src/main/java/com/example/btek/myapplication/NewSplitActivity.kt
@@ -2,11 +2,23 @@ package com.example.btek.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import kotlinx.android.synthetic.main.activity_new_split.*
 
 class NewSplitActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_split)
+
+        start_split_btn.setOnClickListener(::startSplitActivity)
+    }
+
+    fun startSplitActivity(view: View) {
+        val splitName = split_label_txt.text
+
+        Snackbar.make(view, "Will start split [$splitName]", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
     }
 }
